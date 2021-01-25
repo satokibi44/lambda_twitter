7c99bdb78070ee516f32245ebaa5e16248dd36d7
nanasesaitoh <nanasesaitoh@gmail.com/usr/local/bin/python3>
2021-01-25 17:27:32 +0900

    change api

--- lambda_function.py
+++ lambda_function.py
@@ -9,6 +9,7 @@ def lambda_handler(event, context):
     if len(tweet_id_list) != 0:
         for i in range(len(tweet_id_list)):
             tweetUtil.excute_reply(tweet_text_list[i], tweet_id_list[i])
+
     if len(reply_tweet_id_list) != 0:
         for i in range(len(reply_tweet_id_list)):
             tweetUtil.execute_calculate_kusoripuscore(reply_text_list[i], str(reply_tweet_id_list[i]))
