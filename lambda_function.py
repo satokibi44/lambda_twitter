a9b209fe571dc227a765592c29a709a398c119be
nanasesaitoh <nanasesaitoh@gmail.com/usr/local/bin/python3>
2021-01-25 17:31:37 +0900

    cold start 対策

--- lambda_function.py
+++ lambda_function.py
@@ -9,10 +9,19 @@ def lambda_handler(event, context):
     if len(tweet_id_list) != 0:
         for i in range(len(tweet_id_list)):
             tweetUtil.excute_reply(tweet_text_list[i], tweet_id_list[i])
+    else:
+        url = "https://2xa3k3mfyb.execute-api.us-east-2.amazonaws.com/dev/kusoripu-transformer-master-api"
+        param = {'text': 'test'}
+        res = requests.post(url, data=json.dumps(param))
 
     if len(reply_tweet_id_list) != 0:
         for i in range(len(reply_tweet_id_list)):
             tweetUtil.execute_calculate_kusoripuscore(reply_text_list[i], str(reply_tweet_id_list[i]))
+    else:
+        url = "https://2xa3k3mfyb.execute-api.us-east-2.amazonaws.com/dev/kusoripu-bert-master-api"
+        param = {'text': 'test'}
+        res = requests.post(url, data=json.dumps(param))
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
