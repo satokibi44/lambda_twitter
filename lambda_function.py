d6e4da6b9249485c5371dba53440ea094a9e4fca
nanasesaitoh <nanasesaitoh@gmail.com/usr/local/bin/python3>
2021-01-25 14:45:18 +0900

    fix bug

--- lambda_function.py
+++ lambda_function.py
@@ -11,8 +11,8 @@ def lambda_handler(event, context):
             tweetUtil.excute_reply(tweet_text_list[i], tweet_id_list[i])
     if len(reply_tweet_id_list) != 0:
         for i in range(len(reply_tweet_id_list)):
-            tweetUtil.execute_calculate_kusoripuscore(reply_text_list[i], reply_tweet_id_list[i])
-    
+            tweetUtil.execute_calculate_kusoripuscore(
+                reply_text_list[i], reply_tweet_id_list[i])
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
