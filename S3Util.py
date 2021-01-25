fb71a59c86b00503185e775091daee1ee81e0185
nanasesaitoh <nanasesaitoh@gmail.com/usr/local/bin/python3>
2021-01-25 13:32:27 +0900

    add クソリプ度計算

--- S3Util.py
+++ S3Util.py
@@ -4,9 +4,13 @@ class S3Util():
         self.s3 = boto3.client('s3')
         self.BUCKET_NAME = 'kusoripu02'
 
-    def read_latest_tweet_id(self):
-        file_name = 'latest_tweet_id.txt'
+    def read_latest_tweet_id(self,file_name):
         content = self.s3.get_object(Bucket=BUCKET_NAME, Key=file_name)
         body = content['Body'].read()  # b'テキストの中身'
         latest_tweet_id = body.decode()
         return latest_tweet_id
+
+    def write_latest_tweet_id(self, file_name, latest_tweet_id):
+        bucket = self.s3.Object(self.BUCKET_NAME, file_name)
+        bucket.put(Body=str(latest_tweet_id))
+        return
