2786bc30411a72b164c8bf0f583e3b0bb70dcfb8
nanasesaitoh <nanasesaitoh@gmail.com/usr/local/bin/python3>
2021-05-25 23:02:23 +0900

    add User Registry

--- code/UserRegistry.py
+++ code/UserRegistry.py
@@ -1,6 +1,7 @@
 from Utils.TweetUtil import TweetUtil
 from Utils.SqlUtil import SqlUtil
 from TweetFormetter import TweetFormetter
+from Utils.JsonUtil import JsonUtil
 
 class UserRegistry():
     def __init__(self) -> None:
@@ -11,11 +12,12 @@ class UserRegistry():
     def user_manager(self):
         tweet_util = TweetUtil()
         tweet_formatter = TweetFormetter()
+        json_util =JsonUtil()
         timelines = tweet_util.get_reply(
             '"Hey!クソリプbot，クソリプを送って" and "Hey!クソリプbot，クソリプを送らないで"')
         latest_registration_tweetid = self.sql_util.select_latestid(
             "latest_registration_twitterid")
-        
+        timelines = json_util.sort_reply_with_id(timelines)
         for tweet in timelines:
             if (tweet['in_reply_to_user_id'] == tweet_util.my_twitter_id):
                 tweet_text = tweet_formatter.screening(tweet['text'])
@@ -27,7 +29,12 @@ class UserRegistry():
 
                 elif (tweet_text == "Hey!クソリプbot，クソリプを送らないで"):
                     self.sql_util.delete_twitterid(tweet['user']['id'])
-
+        else:
+            tweet = timelines[-1]
+            latest_registration_tweetid = tweet['id']
+            self.sql_util.insert_latestid(
+                "latest_registration_twitterid", latest_registration_tweetid)
+    
     def add_user(self):
         tweet_util = TweetUtil()
         timelines = tweet_util.get_reply("Hey!クソリプbot，クソリプを送って")
--- code/test.py
+++
@@ -1,5 +0,0 @@
-from UserRegistry import UserRegistry
-
-user_registry = UserRegistry()
-
-user_registry.user_manager()
